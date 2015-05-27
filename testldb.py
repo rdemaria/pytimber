7b6014d000c48cb05867b14aacf56e3a25618da8
Riccardo De Maria <riccardodemaria@gmail.com>
2015-05-27 14:43:16 +0200

    update test

--- testldb.py
+++ testldb.py
@@ -3,21 +3,22 @@ import pytimber
 ldb=pytimber.LDB()
 
 
-ldb.search('HX:%')
+print ldb.search('HX:BETA%')
 
 t1='2015-05-13 00:00:00.000'
 t2='2015-05-15 00:00:00.000'
 d=ldb.get('HX:FILLN',t1,t2)
-d=ldb.get('HX:BMODE_%',t1,t2)
+print d
 
 t1='2015-05-13 12:00:00.000'
 t2='2015-05-13 12:00:01.000'
 d=ldb.get('LHC.BQBBQ.CONTINUOUS_HS.B1:ACQ_DATA_H',t1,t2)
 
+print d
 
 
 ldb.tree
-dir(ldb.tree)
-ldb.tree.LHC.Collimators.BPM.bpmColl.get_vars()
+print dir(ldb.tree)
+print ldb.tree.LHC.Collimators.BPM.bpmColl.get_vars()
 
 
