37fbb2c6149ea7befa3399eb307656603edfb07b
Cédric Hernalsteens <cedric.hernalsteens@cern.ch>
2016-02-16 13:41:57 +0100

    Test for the fundamentals

--- tests/test_fundamentals.py
+++ tests/test_fundamentals.py
@@ -4,5 +4,5 @@ from datetime import datetime, timedelta
 log = pytimber.LoggingDB(source='all')
 
 now = datetime.now()
-fundamental = 'CPS:TOF**:TOF'
-log.get('CPS:TGM%', now, now + timedelta(hours = -1), fundamental)
+fundamental = 'CPS:%:SFTPRO%'
+log.get('CPS.TGM:USER%', now - timedelta(minutes = 10), now, fundamental)
