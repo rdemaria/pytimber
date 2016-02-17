2eb2a9d36ad7cc4c1a00f20493f9233fd658fdd0
Tom Levens <tom.levens@cern.ch>
2016-02-17 05:48:09 +0100

    Improved timestamp test

--- tests/test_ldb.py
+++ tests/test_ldb.py
@@ -1,40 +1,51 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import logging
 
+# should be done before importing pytimber
 logging.basicConfig(level=logging.INFO)
 
 import pytimber
 
 ldb=pytimber.LoggingDB(source='ldb')
 
+print('------------------------------------------------------------------------')
+
 print(ldb.search('HX:BETA%'))
+
 print('------------------------------------------------------------------------')
 
 t1='2015-05-13 00:00:00.000'
 t2='2015-05-15 00:00:00.000'
-d=ldb.get('HX:FILLN',t1,t2)
+d=ldb.get('HX:FILLN', t1, t2)
 print(d)
+
 print('------------------------------------------------------------------------')
 
-d=ldb.get('HX:FILLN',t1,t2,unixtime=True)
+d=ldb.get('HX:FILLN', t1, t2, unixtime=True)
 print(d)
+
 print('------------------------------------------------------------------------')
 
 t1='2015-05-13 12:00:00.000'
 t2='2015-05-13 15:00:00.000'
-d=ldb.get(ldb.search('HX:BETA%'),t1,t2,unixtime=True)
+d=ldb.get(ldb.search('HX:BETA%'), t1, t2, unixtime=True)
 print(d)
+
 print('------------------------------------------------------------------------')
 
 t1='2015-05-13 12:00:00.000'
 t2='2015-05-13 12:00:01.000'
-d=ldb.get('LHC.BQBBQ.CONTINUOUS_HS.B1:ACQ_DATA_H',t1,t2)
+d=ldb.get('LHC.BQBBQ.CONTINUOUS_HS.B1:ACQ_DATA_H', t1, t2)
 print(d)
+
 print('------------------------------------------------------------------------')
 
 print(dir(ldb.tree))
+
 print('------------------------------------------------------------------------')
 
 print(ldb.tree.LHC.Collimators.BPM.bpmColl.get_vars())
+
 print('------------------------------------------------------------------------')
-
-
