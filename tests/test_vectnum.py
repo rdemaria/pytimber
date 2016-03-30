5dfbd950b47a2c1feef1e27d97195b49e665e8ba
Riccardo De Maria <riccardodemaria@gmail.com>
2016-03-30 18:37:43 +0200

    Update vecnum test

--- tests/test_vectnum.py
+++ tests/test_vectnum.py
@@ -14,10 +14,10 @@ t1 = '2016-03-28 00:00:00.000'
 t2 = '2016-03-28 23:59:59.999'
 
 
-d = ldb.get('LHC.BOFSU:BPM_NAMES_H', t1)
+d = ldb.get('LHC.BOFSU:BPM_NAMES_H', t1, t2)
 
 t1 = '2016-03-28 04:50:00.000'
 t2 = '2016-03-28 05:01:00.000'
-ldb.get('%BSRT.%FIT_POSITION_H',t1,'2016-03-28 05:00:00.000')
+d=ldb.get('LHC.BSRT.5R4.B1:FIT_POSITION_H',t1,t2)
 
 
