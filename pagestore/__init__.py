d68d59276635d95c52a630c0eb78f2bc9acb5b0c
Riccardo De Maria <riccardodemaria@gmail.com>
2016-06-08 09:23:42 +0200

    moving helper classes to pytimber

--- pagestore/__init__.py
+++ pagestore/__init__.py
@@ -1,6 +1,4 @@
-__version__="0.0.15"
+__version__="0.0.16"
 
 from .pagestore import PageStore
 from .page import Page
-from .dataquery import DataQuery, parsedate, dumpdate, flattenoverlap, set_xaxis_date, set_xaxis_utctime, set_xlim_date, get_xlim_date
-from . import timberdata
