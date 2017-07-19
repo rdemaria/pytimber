7a2e8fdf2841c804366a0ebb2bd3f8f3ac970683
Riccardo De Maria <riccardodemaria@gmail.com>
2017-07-20 00:00:45 +0200

    Update copyright year

--- pytimber/pytimber.py
+++ pytimber/pytimber.py
@@ -3,7 +3,7 @@
 '''
 PyTimber -- A Python wrapping of CALS API
 
-Copyright (c) CERN 2015-2016
+Copyright (c) CERN 2015-2017
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of
 this software and associated documentation files (the "Software"), to deal in
