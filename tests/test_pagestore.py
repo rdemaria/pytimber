3b1de4a362de67c6f08f26371df1152d1e39e2d2
Riccardo De Maria <riccardodemaria@gmail.com>
2019-12-06 16:25:37 +0100

    adding tests

---
+++ tests/test_pagestore.py
@@ -0,0 +1,21 @@
+from numpy import *
+from pytimber.pagestore import *
+
+
+"""
+def test_list():
+    try:
+      db=PageStore('test.db','testdata')
+      data={'v1':([1,2,3],[4,5,6]),
+        'v2':([1,2,3],[4,5,6])}
+
+      db.store(data)
+      data1=db.get(['v1','v2'])
+      data2=db.get('v%')
+
+      assert set(data1.keys())==set(data1.keys())
+      assert data['v1'][0]==db.get('v1')['v1']
+    finally:
+      db.delete()
+"""
+
