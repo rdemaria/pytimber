5dcb0c9fd8f71a9d639f3985874fd499b441e753
Riccardo De Maria <riccardodemaria@gmail.com>
2020-05-19 13:54:28 +0200

    adding yarn example

---
+++ examples/nxcals_standalone.py
@@ -0,0 +1,48 @@
+import cmmnbuild_dep_manager
+import jpype
+import  jpype.imports
+
+mgr=cmmnbuild_dep_manager.Manager()
+mgr.start_jpype_jvm()
+jpype.imports.registerDomain("cern")
+
+from java.util import HashMap
+
+from cern.nxcals.common.utils.SparkUtils \
+        import createSparkSession, createSparkConf
+from cern.nxcals.api.config import SparkProperties
+from cern.nxcals.api.extraction.data.builders import DataQuery
+
+props={"spark.executor.memory":"2G",
+       "spark.executor.cores":"10",
+       "spark.yarn.appMasterEnv.JAVA_HOME":"/var/nxcals/jdk1.8.0_121",
+       "spark.executorEnv.JAVA_HOME":"/var/nxcals/jdk1.8.0_121",
+       "spark.yarn.jars":"hdfs:////project/nxcals/lib/spark-2.4.0/*.jar",
+       "spark.yarn.am.extraLibraryPath":"/usr/lib/hadoop/lib/native",
+       "spark.executor.extraLibraryPath":"/usr/lib/hadoop/lib/native",
+       "spark.yarn.historyServer.address":"ithdp1001.cern.ch:18080",
+       "spark.yarn.access.hadoopFileSystems":"nxcals",
+       "spark.sql.caseSensitive":"true"}
+
+spark_properties=SparkProperties()
+spark_properties.setAppName("PYTIMBER3")
+spark_properties.setMasterType("yarn")
+spark_properties.setProperties(props)
+spark_conf=createSparkConf(spark_properties)
+spark=createSparkSession(spark_conf)
+
+
+dq=( DataQuery.builder(spark).byEntities()
+    .system("CMW")
+    .startTime("2018-04-29 00:00:00.000")
+    .endTime("2018-04-30 00:00:00.000")
+    .entity()
+    .keyValue("device", "LHC.LUMISCAN.DATA")
+    .keyValue("property", "CrossingAngleIP1")
+    .buildDataset()
+)
+
+rows = df.collect()
+
+
+
